a64205d192b0df4d597dab3115c9137f404ae615
sunny <sengersunny448@gmail.com>
2023-06-14 00:25:55 +0530

    body component

--- Component/Body.jsx
+++ Component/Body.jsx
@@ -1,48 +1,55 @@
 import { useState } from "react";
-import { restaurantList  } from "./config";
+import { restaurantList } from "./config";
 import RestrauCard from "./RestrauCard";
 
-
-
-const Body = () =>{
-
-   // const newKeyWord="KFC";
-   const [newKeyWord , setNewKeyWord]=useState();
-   const [restrau, setrestrau]=useState(restaurantList);
-   console.log(restrau);
-   // now we have to apply the filter algorithm
-   function filterData(setNewKeyWord,restrau){
-      console.log(restrau);
-     const filterData = (restrau) =>{
-         console.log(restrau.data.name);
-         restrau.data.name.include(newKeyWord);
-      }
-      return filterData;
+// now we have to apply the filter algorithm
+function filterData(newKeyWord, restrauntData) {
+   const filterDataGet = restrauntData.filter(
+      (res) =>{
+    res.data.name.includes(newKeyWord);
    }
-
-
-    return(
-      <>      
-      <div className="search-container">
-         <input type="text" className="search-input" 
-         placeholder="search"
-         value={newKeyWord} onChange={(e)=>{
-            setNewKeyWord(e.target.value) ;
-         }}/>
-         <button className="searchbtn" onClick={ ()=>{
-            // filter on click 
-          const Data = filterData(setNewKeyWord,restrau);
-         //  now we have to update the data 
-         setrestrau(Data); 
-         }}>search</button>
-      </div>  
-       <div className="cardInfo">
-          {restrau.map((restaurant) => {
-         return <RestrauCard key={restaurant.data.id} {...restaurant.data} />;
-       })}
-       </div>
-       </>
-
-    );
+  );
+  return filterDataGet;
+  
  }
- export default Body;
+
+const Body = () => {
+  // useState: To create a state variable, searchText is local state variable
+  const [newKeyWord, setNewKeyWord] = useState(" ");
+  const [restrauntData, setrestrautData] = useState(restaurantList);
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="search"
+          value={newKeyWord}
+          onChange={(e) => {
+            setNewKeyWord(e.target.value);
+          }}
+        />
+        <button
+          className="searchbtn"
+          onClick={() => {
+            // filter on click
+            const data = filterData(newKeyWord, restrauntData);
+            // update the state of restaurants list
+            setrestrautData(data);
+          }}
+        >
+          search
+        </button>
+
+      </div>
+
+      <div className="cardInfo">
+        {/* how the map function work  */}
+        {restrauntData.map((restaurant) => {
+          return <RestrauCard key={restaurant.data.id} {...restaurant.data} />;
+        })}
+      </div>
+    </>
+  );
+};
+export default Body;
