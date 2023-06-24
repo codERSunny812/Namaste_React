96ed5433f30b89b16307dfcdbe076f70cacd398a
sunny <sengersunny448@gmail.com>
2023-06-24 10:58:22 +0530

    main js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import  ReactDOM ,{createRoot} from "react-dom/client";
 import HeaderComponent  from "../Component/HeaderComponent";
 import Body  from "../Component/Body";
 import Footer from "../Component/Footer";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import About from "../Component/About";
+import ErrorPage from "../Component/ErrorPage";
+import ContactUs from "../Component/ContactUs";
+import RestrauDetail from "../Component/RestrauDetails";
+
 
 
 
@@ -13,12 +19,43 @@ const AppLayout = () =>{
    return(
       <>
       <HeaderComponent />
-      <Body />
+      <Outlet />
       <Footer />
       </>
    );
 }
+
+const appBrowser=createBrowserRouter(
+   [
+      {
+         path:'/',
+         element:<AppLayout />,
+         errorElement:<ErrorPage/>,
+         children:[
+            {
+               path:"/",
+               element:<Body/>   
+            },
+            {
+               path:"/aboutus",
+               element:<About />
+            },
+            {
+               path:"/contactus",
+               element:<ContactUs />
+            },
+            {
+               path:"/restaurant/:id",
+               element:<RestrauDetail /> 
+
+            }
+
+         ]
+      },
+     
+   ]
+)
 // selected the root element 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // here we render this on our page 
-root.render(<AppLayout />);          
+root.render(<RouterProvider router={appBrowser} />);          
